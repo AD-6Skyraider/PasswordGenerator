a370997cb98a6a9591eb7aa70a97b37f5ea96a54
Don Vest <tsukasasan@hotmail.com>
2021-10-28 13:36:50 -0500

    Completed initial version of project.

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 
 // Character Generator Functions
 
-// Function that accepts a string value as an anrgument and returns a random index number from a string argument.
+// Function that accepts a string value as an argument and returns a random index number from a string argument.
 function randomIndex(str){
     return Math.floor(Math.random() * str.length);
 }
-randomIndex(`example`); // 0, 1, 2, 3, 4, 5, 6
+
 // Example of the randomIndex function
-console.log(randomIndex(`example`));
+console.log(randomIndex(`example`)); // 0, 1, 2, 3, 4, 5, 6
 
 // Function that returns a random lowercase letter.
 function getRandomLower(){
@@ -29,7 +29,7 @@ function getRandomUpper(){
 }
 
 // Example of the getRandomUpper function
-console.log(getRandomUpper());
+console.log(getRandomUpper()); // Random uppercase letter
 
 // Function that returns a random number as a string value. 
 function getRandomNumber (){
@@ -81,7 +81,7 @@ let generatedPassword = ``;
 // 2. FILTER OUT UNCHECKED OPTIONS
 // True and false values can be added together. True is 1 and false is zero.
 // NOTE: The value set to typesCount will be used when building the password.
-const typesCount = lower = upper = number = symbol;
+const typesCount = lower + upper + number + symbol;
 console.log(typesCount);
 
 // If the user has not selected any of the four options, then display alert and return an empty string from the function so the password displayed will just be an empty string.
@@ -92,28 +92,28 @@ if (typesCount === 0){
 }
 
 // Creating an array of arrays.The first item in each nested array hold the value of a string that will be used to access a function in the randomFunctions object. Also, the second item in each nested array is one of the values passed into this generatePassword function.
-let typesARR = [
+let typesArr = [
     [`lower`, lower],
     [`upper`, upper],
     [`number`, number],
     [`symbol`, symbol]
 ];
-console.log(typesARR);
+console.log(typesArr);
 
 // The filter method creates a new array with all the items that pass the test implemented by the provided function (All the items that cause the function to return a boolean value of true when the function is run using the item as an argument for the item parameter in this example..)
 // Checking for the value for index of one in each item in the array if it is false.
-typesARR = typesARR.filter(item => {
+typesArr = typesArr.filter(item => {
     console.log(item[1]);
     return item[1];
 });
-console.log(`typesARR:`, typesARR);
+console.log(`typesArr:`, typesArr);
 
 // 3. LOOP OVER THE LENGTH AND CALL THE GENERATOR FUNCTION FOR EACH CHECKED OPTION
 // Building password with a for loop
 // NOTE: The value for "length" is the value selected for the length number input.
 for (i = 0; i < length; i += typesCount){
     //  One of the items in the updated/filtered version of the typesARR will be the value/argument passed into for the type parameter each time the anonymous arrow function.
-    typesARR.forEach(type => {
+    typesArr.forEach(type => {
         const funcName = type[0];
         console.log(funcName);
         // Accessing and running/executing a function in the randomFunctions object. Also, adding the value returned from the accessed function to the generatedPassword string variable.
@@ -124,8 +124,9 @@ for (i = 0; i < length; i += typesCount){
 
 // 4. ADD THE GENERATED PASSWORD TO THE FINAL VARIABLE AND RETURN IT FROM THE FUNCTION.
 // Removing extra characters if necessary (the above loop will create a password that may not match the length selected if that length is not a multiple of the number of options/checkboxes selected).
-const finalPassword = generatedPassword.slice(0,length);
+const finalPassword = generatedPassword.slice(0, length);
 console.log(finalPassword);
+
 return finalPassword;
 }
 
@@ -143,11 +144,47 @@ generateEL.addEventListener(`click`, () => {
     
     // Accessing the value for the number input and changing the value from a string to a number.
     // NOTE: The value returned from a number input is a string value. 
-    const length = parseInt(lengthEL.Value);
+    const length = parseInt(lengthEL.value);
 
     console.log(hasLower, hasUpper, hasNumber, hasSymbol, length);
 
 // The generatePassword function takes the true false values determined by the checkboxes as well as the number input as arguments and returns a string (the password) which is set as the innerText value for the "result" (span) element. 
 resultEL.innerText = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length);
+});
+
+// COPY PASSWORD
+clipboardEL.addEventListener(`click`, () => {
+    // Creating a textArea element which will be used to put the password inside of so that it can be selected/copied.
+    const textarea = document.createElement(`textarea`);
+
+    // Accessing the text/string value for the result span and setting it to the password variable. 
+    const password = resultEL.innerText;
+
+    // If user clicks the clipboard while no password is displayed, the function will end and nothing will be copied to the clipboard.
+    if (password === ``){
+        alert (`Please generate a password first`);
+        return;
+    }
+
+    // Setting the value for the textarea to the password that is currently being displayed to the user.
+    textarea.value = password;
+
+    // Selecting the body element
+    const body = document.querySelector(`body`);
+
+    // Adding the textarea to the webpage/document.
+    body.append(textarea);
+
+    // Using the select method which selects (AKA Focuses in on) an element. This is will highlight/select the value (AKA Password) inside the textarea.
+  textarea.select();
+
+  // Using execCommand to copy the selected value to the clipboard on the device the webpage is being viewed on
+  // NOTE: Some execCommand commands/arguments are not supported by all browsers. However, copy is an execCommand command that all browsers support.
+  document.execCommand(`copy`);
+
+  // Removing the textarea element from the webpage/document
+  textarea.remove();
+  
+  alert(`Password has been copied to the clipboard`);
 });
 
